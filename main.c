60828d5fa352a6212e0e6365af3b3a26a96b79ec
David <david@achoy.net>
2023-10-24 15:47:46 -0600

    create func

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <stdbool.h>
 // Constants
 typedef struct {
     int num_files;
@@ -24,16 +24,39 @@ typedef struct {
 
 // Function Prototypes
 void create(const char *archive_name, char *files[], int num_files);
-void extract(char *archive_name);
-void list(char *archive_name);
+void extract(const char *archive_name, const char *file_name);
+void list(const char *archive_name);
 void delete(char *archive_name, const char *file_to_delete);
 void update (char *archive_name, char *files[], int num_files);
 void append(char *archive_name, char *files[], int num_files);
 void defragment(char *archive_name);
 void verbose(char *archive_name);
-
+bool find_file_info(FILE *archive, const char *file_name, FileInfo *file_info);
+//auxiliary functions
+bool find_file_info (
+    FILE *archive, 
+    const char *file_name, 
+    FileInfo *file_info
+) {
+    // leer metadata
+    ArchiveMetadata metadata;
+    fread(&metadata, sizeof(ArchiveMetadata), 1, archive);
+    // buscar el archivo
+    for (int i = 0; i < metadata.num_files; i++) {
+        // leer información de archivo
+        fread(file_info, sizeof(FileInfo), 1, archive);
+        // comparar nombre de archivo
+        if (strcmp(file_info->filename, file_name) == 0) {
+            return true;
+        }
+        // saltar contenido de archivo
+        fseek(archive, file_info->file_size, SEEK_CUR);
+    }
+    // no se encontró el archivo
+    file_info->filename[0] = '\0';
+    return false;
+}
 // create function
-
 void create(
     const char *archive_name,  // Nombre del archivo de destino
     char *files[],             // Arreglo de nombres de archivos para incluir en el archivo
@@ -61,7 +84,12 @@ void create(
         int file_size = ftell(file);
         fseek(file, 0, SEEK_SET);
         // Escribir información de archivo
-        FileInfo file_info = {files[i], file_size, ftell(archive)};
+        FileInfo file_info;
+        strncpy(file_info.filename, files[i], 255);
+        file_info.filename[255 - 1] = '\0';
+        file_info.file_size = file_size;
+        file_info.start_position = ftell(archive);
+        printf("file_info.start_position: %d\n", file_info.start_position);
         fwrite(&file_info, sizeof(FileInfo), 1, archive);
         // Escribir contenido del archivo
         char *buffer = malloc(file_size);
@@ -74,6 +102,80 @@ void create(
     // Cerrar archivo
     fclose(archive);
 }
-int main () {
+
+// list function
+void list(
+    const char *archive_name
+) {
+    // Abrir archivo
+    FILE *archive = fopen(archive_name, "rb");
+    if (!archive) {
+        printf("Error al abrir el archivo %s\n", archive_name);
+        return;
+    }
+    // Leer metadata
+    ArchiveMetadata metadata;
+    fread(&metadata, sizeof(ArchiveMetadata), 1, archive);
+
+    //listar archivos
+    for (int i = 0; i < metadata.num_files; i++) {
+        // Leer información de archivo
+        FileInfo file_info;
+        fread(&file_info, sizeof(FileInfo), 1, archive);
+        // Imprimir nombre de archivo
+        printf("%s\n", file_info.filename);
+        // Saltar contenido de archivo
+        fseek(archive, file_info.file_size, SEEK_CUR);
+    }
+    fclose(archive);
+}
+
+// extract function
+void extract(
+    const char *archive_name, 
+    const char *file_name
+) {
+    // Abrir archivo
+    FILE *archive = fopen(archive_name, "rb");
+    if (!archive) {
+        printf("Error al abrir el archivo %s\n", archive_name);
+        return;
+    }
+
+    // buscar el archivo
+    FileInfo file_info;
+    if(!find_file_info(archive, file_name, &file_info)) {
+        printf("No se encontró el archivo %s\n", file_name);
+        fclose(archive);
+        return;
+    }
+    FILE *output = fopen(file_name, "wb");
+    if (!output) {
+        printf("Error al abrir el archivo %s\n", file_name);
+        fclose(archive);
+        return;
+    }
+
+    // Leer contenido de archivo
+    fseek(archive, file_info.start_position, SEEK_SET);
+    char buffer[1024];
+    int bytes_left = file_info.file_size;
+
+    // Escribir contenido de archivo
+    while (bytes_left > 0)
+    {
+        int bytes_to_read = bytes_left < sizeof(buffer) ? bytes_left : sizeof(buffer);
+        fread(buffer, bytes_to_read, 1, archive);
+        fwrite(buffer, bytes_to_read, 1, output);
+        bytes_left -= bytes_to_read;
+    }
+    // Cerrar archivos
+    fclose(archive);
+    fclose(output);
+}
+
+int main() {
+    char *files[] = {"file1.txt", "file2.txt"};
+    create("archive.bin", files, 2);
     return 0;
 }
